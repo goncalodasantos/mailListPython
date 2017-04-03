3fe17ac6ab660620fab26b0a9a5bf79c657403a4
Gonçalo <goncalodasantos@gmail.com>
2017-04-04 00:18:10 +0100

    final messaging

--- mailtest.py
+++ mailtest.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+me = "mail@gmail.com"
+my_password = r"password"
+
+numberOfEmailsByBatch=50
+sleepTimeBetweenBatches=120
 
 
 
@@ -12,27 +23,50 @@ file2 = open("recipients.txt", "r")
 toaddrs  = file2.read().split()
 
 
-fromaddr = 'mail@gmail.com'
-
-
-msg = "\r\n".join([
-  "From: Gonçalo Santos",
-  "Subject: Just a message",
-  message
-  ])
 
 
 
-username = 'email@gmail.com'
-password = 'mailpassword'
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(username,password)
 
+
+
+
+# Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+s = smtplib.SMTP_SSL('smtp.gmail.com')
+# uncomment if interested in the actual smtp conversation
+# s.set_debuglevel(1)
+# do the smtp auth; sends ehlo if it hasn't been sent already
+s.login(me, my_password)
+
+
+i=0
 for c in toaddrs:
-	server.sendmail(fromaddr,c,msg)
-	print "Mail Send Successfully to", c
 
-server.quit()
+
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = "Message Test"
+	msg['From'] = me
+	msg['To'] = c
+	html = message+" "+str(i)
+	part2 = MIMEText(html, 'html')
+
+	msg.attach(part2)
+
+	if(i%numberOfEmailsByBatch==0 and i!=0):
+		j=0
+		while(j<sleepTimeBetweenBatches):
+			if(j%10==0):
+				print "Sleeping for ",str(sleepTimeBetweenBatches-j)," more seconds"
+			time.sleep(10)
+			j=j+10
+
+
+	s.sendmail(me, c, msg.as_string())
+	print i," Mail Send Successfully to", c
+
+	i=i+1
+
+
+
+
+s.quit()
